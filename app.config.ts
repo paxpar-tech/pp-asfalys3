68d626ef8dc21dfb82ac8ec7a1921772d8b04f28
Philippe Entzmann <philippe.entzmann@gmail.com>
2023-10-11 14:39:06 +0200

    fix: wip

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,7 @@ export default defineAppConfig({
     title: 'Docus',
     description: 'The best place to start your documentation.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+
     socials: {
       twitter: 'nuxt_js',
       github: 'nuxt-themes/docus',
@@ -12,6 +13,7 @@ export default defineAppConfig({
         href: 'https://nuxt.com'
       }
     },
+
     github: {
       dir: '.starters/default/content',
       branch: 'main',
@@ -19,20 +21,30 @@ export default defineAppConfig({
       owner: 'nuxt-themes',
       edit: true
     },
+
     aside: {
       level: 0,
       collapsed: false,
       exclude: []
     },
+
     main: {
       padded: true,
       fluid: true
     },
+
     header: {
       logo: true,
       showLinkIcon: true,
       exclude: [],
       fluid: true
+    },
+
+    footer: {
+      credits: {
+        text: 'Powered by paxpar.tech',
+        href: 'https://paxpar.tech'
+      }
     }
   }
 })
